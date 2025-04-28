ebac421a92ce92d7c63e4d523392e42f3e0882ae
SinaSNP <ssasanpour99@gmail.com>
2025-04-28 04:24:26 +0300

    nextjs err 3

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -10,11 +10,10 @@ export async function DELETE(req: Request, { params }: { params: { slug: string 
   return NextResponse.json({ message: "Deleted" });
 }
 
-export async function POST(req: Request, context: { params: { slug: string } }) {
+export async function POST(req: Request, { params }: { params: { slug: string } }) {
   const formData = await req.formData();
   const title = formData.get('title') as string;
   const content = formData.get('content') as string;
-  const { params } = context; // Access params from the context
 
   await connectToDB();
   await Post.updateOne({ slug: params.slug }, { title, content });
